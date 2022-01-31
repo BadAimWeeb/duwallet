963001316de22d9bce3c8e44652032d2d258d00c
Lê Quang Lâm <badaimweeb@protonmail.com>
2022-01-31 14:21:34 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Navigate } from "react-router-dom";
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -10,16 +10,35 @@ import './index.css';
 
 import Header from "./Header";
 
+import CLogic from "./CLogic";
 import reportWebVitals from './reportWebVitals';
+
+let cLogicInstance = new CLogic();
+(globalThis as any).cLogic = cLogicInstance;
+
+let extDOM;
+let readStatus = cLogicInstance.readData();
+switch (readStatus) {
+  case "ENCRYPTED":
+    extDOM = (
+      <Navigate to="/encrypted" />
+    )
+    break;
+  case "MALFORMED":
+    
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Header />
+      {extDOM}
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
