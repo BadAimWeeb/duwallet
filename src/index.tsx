65b0153e21bff5d22ded3ee5eea41bc830cb3ebe
BadAimWeeb <badaimweeb@protonmail.com>
2022-02-01 18:35:47 +0700

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Route } from "react-router-dom";
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -9,6 +9,9 @@ import '@fontsource/roboto/700.css';
 import './index.css';
 
 import Header from "./Header";
+
+// Pages
+import Pages_Encrypted from "./pages/Encrypted";
 
 import CLogic from "./CLogic";
 import reportWebVitals from './reportWebVitals';
@@ -25,7 +28,10 @@ switch (readStatus) {
     )
     break;
   case "MALFORMED":
-    
+    extDOM = (
+      <Navigate to="/malformed" />
+    )
+    break;
 }
 
 ReactDOM.render(
@@ -33,6 +39,9 @@ ReactDOM.render(
     <Router>
       <Header />
       {extDOM}
+      <Route path={"/encrypted"} >
+        <Pages_Encrypted />
+      </Route>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
