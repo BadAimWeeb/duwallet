487e8bb1fcf3a5e3fdc809f2a6098b602aef2e97
Lê Quang Lâm <badaimweeb@protonmail.com>
2022-08-20 01:27:30 +0000

    Polyfilling node modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+
+import rollupNodePolyfills from 'rollup-plugin-node-polyfills'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()]
+    base: './',
+    plugins: [react()],
+    resolve: {
+        alias: {
+            buffer: 'buffer/'
+        }
+    },
+    optimizeDeps: {
+        // Polyfilling node modules in development
+        esbuildOptions: {
+            define: {
+                global: "globalThis"
+            },
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    process: true,
+                    buffer: true
+                }),
+                NodeModulesPolyfillPlugin()
+            ]
+        }
+    },
+    build: {
+        rollupOptions: {
+            plugins: [
+                // Polyfilling node modules in production
+                rollupNodePolyfills()
+            ]
+        }
+    }
 })
